2add5ad855df388467b57cae0d1a8026c52bf82b
Christian Juner <christian.juner@viison.com>
2014-11-17 13:46:38 +0100

    test

--- src/Viison/ComposerRulesPlugin/Plugin.php
+++ src/Viison/ComposerRulesPlugin/Plugin.php
@@ -5,13 +5,29 @@ namespace Viison\ComposerRulesPlugin;
 use Composer\Composer;
 use Composer\IO\IOInterface;
 use Composer\Plugin\PluginInterface;
+use Composer\Plugin\PluginEvents;
+use Composer\EventDispatcher\EventSubscriberInterface;
 
-class Plugin implements PluginInterface {
+class Plugin implements PluginInterface, EventSubscriberInterface {
 
     public function activate(Composer $composer, IOInterface $io)
     {
         $installer = new Installer($io, $composer);
         $composer->getInstallationManager()->addInstaller($installer);
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return array(
+            PluginEvents::COMMAND => array(
+                array('onCommand', 0)
+            ),
+        );
+    }
+
+    public function onCommand(Composer\Plugin\CommandEvent $event)
+    {
+        echo __METHOD__, ' ', $event->getCommandName(), " ################ \n";
     }
 
 }
