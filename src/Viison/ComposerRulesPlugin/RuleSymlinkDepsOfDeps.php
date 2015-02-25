59be58e28ba3284fe35806a5127fd2d75daf3d1d
Christian Juner <christian.juner@viison.com>
2015-02-25 12:02:20 +0100

    Fix usage of non-existent variable

--- src/Viison/ComposerRulesPlugin/RuleSymlinkDepsOfDeps.php
+++ src/Viison/ComposerRulesPlugin/RuleSymlinkDepsOfDeps.php
@@ -2,7 +2,7 @@
 /**
  * VIISON/composer-rules-plugin
  *
- * Copyright (c) 2014 VIISON GmbH
+ * Copyright (c) 2014-2015 VIISON GmbH
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
@@ -207,7 +207,8 @@ class RuleSymlinkDepsOfDeps extends EmptyRule {
 
         $linkDir = dirname($link);
         if (!is_dir($linkDir)) {
-            $this->logger->logMethodStep(__METHOD__, array($package->getPrettyName(),
+            $this->logger->logMethodStep(__METHOD__, array(
+                $targetPackage->getPrettyName(),
                 'Installing ' . $linkPackage->getPrettyName()
                 . ' because it is need as a link target.'));
             $this->composer->getInstallationManager()
